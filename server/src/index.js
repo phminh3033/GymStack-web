4e656f70f02a3f5c5c4c55ec10f29e7946bdfe28
phminh3033 <phminh3033@gmail.com>
2022-08-21 23:09:12 +0700

    base project

---
+++ server/src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const URI_CONNECT = 'mongodb+srv://admin:rNJt0EIskQSUjYn1@cluster0.rkkex.mongodb.net/?retryWrites=true&w=majority';
+
+
+
+//Can use middleware
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(bodyParser.json());
+
+//HTTP request logger
+app.use(morgan('combined'));
+
+app.get('/tin-tuc', function (req, res) {
+    res.send('hello, world!')
+  })
+
+//Connect to DB
+mongoose.connect(URI_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to DB");
+        app.listen(PORT, () => {
+            console.log(`listening on port ${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
